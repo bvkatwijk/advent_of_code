0238f0d42bc15299f687da854e497f347e645642
Boris van Katwijk <bvkatwijk@gmail.com>
2023-12-03 09:27:30 +0100

    Add game id function

--- src/aoc_2.rs
+++ src/aoc_2.rs
@@ -3,21 +3,38 @@ use aoc_1;
 #[allow(dead_code)]
 const EXAMPLE_01: &str = "./src/02_01_example.txt";
 
+#[allow(dead_code)]
 fn aoc_2_1(input: &str) -> u32 {
-    return 1;
-    // return aoc_1::file_lines(input)
-    //     .map(|l| l.unwrap())
-    //     .sum();
+    return aoc_1::file_lines(input)
+        .map(|l| l.unwrap())
+        .map(|s| possible_game_id_or_0(&s))
+        .sum();
 }
 
+fn possible_game_id_or_0(input: &str) -> u32 {
+    0
+}
+
+fn game_id(input: &str) -> u8 {
+    return input.split(":")
+        .next()
+        .unwrap()[5..]
+        .parse::<u8>()
+        .unwrap();
+}
 
 
 #[cfg(test)]
 mod tests{
+    use super::*;
     use crate::aoc_2::{aoc_2_1, EXAMPLE_01};
 
     #[test]
     fn aoc_1_2_example_test() {
         assert_eq!(1, aoc_2_1(EXAMPLE_01));
+    }
+
+    fn game_id_test() {
+        assert_eq!(1, game_id("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"));
     }
 }
