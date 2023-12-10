34fc49c39d4a41f9ff4f6946a9de4bb8d698ccb1
Boris van Katwijk <bvkatwijk@gmail.com>
2023-12-10 22:16:55 +0100

    03-01: Closer

--- src/aoc_3.rs
+++ src/aoc_3.rs
@@ -14,7 +14,8 @@ fn aoc_3_1(path: &str) {
 
 // Returns a Vec of Numbers from the given matrix
 fn numbers_in_matrix(matrix: Vec<&str>) -> Vec<Number> {
-    matrix.iter()
+    matrix
+        .iter()
         .enumerate()
         .flat_map(|(index, value)| numbers_in_line(value, index as u8))
         .collect()
@@ -24,7 +25,7 @@ fn numbers_in_matrix(matrix: Vec<&str>) -> Vec<Number> {
 fn numbers_in_line(input: &str, line: u8) -> Vec<Number> {
     input
         .split(|c: char| !c.is_digit(10))
-        .filter(|s| !s.is_empty()) 
+        .filter(|s| !s.is_empty())
         .map(|s| s.parse::<u32>().unwrap())
         .map(|i| {
             let x = input.find(&i.to_string()).unwrap();
@@ -33,7 +34,7 @@ fn numbers_in_line(input: &str, line: u8) -> Vec<Number> {
                 value: i,
                 line: line as usize,
                 start_x: x,
-                end_x: x + (len - 1)
+                end_x: x + (len - 1),
             }
         })
         .collect()
@@ -44,27 +45,39 @@ fn numbers_in_line(input: &str, line: u8) -> Vec<Number> {
 fn adjacent_symbols(grid: &Vec<String>, number: Number) -> Vec<&str> {
     let line_above: usize = match number.line {
         0 => 0,
-        _ => number.line - 1
+        _ => number.line - 1,
     };
-    let line_below = std::cmp::min(grid.len() - 1, number.line + 1);
-    grid[line_above..line_below]
+    println!("line_above {}", line_above);
+    let line_below = std::cmp::min(grid.len() - 1, number.line + 2);
+    println!("line_below {}", line_below);
+    let x_start = match number.start_x {
+        0 => 0,
+        _ => number.start_x - 1,
+    };
+    println!("x_start {}", x_start);
+    let x_end = std::cmp::min(grid[0].len() - 1, number.end_x + 2);
+    println!("x_end {}", x_end);
+    grid[line_above..line_below + 1]
         .iter()
-        .map(|s| &s[number.start_x-1..number.end_x+1])
+        .map(|s| &s[x_start..number.end_x])
+        .map(|s| {
+            println!("s: {}", &s);
+            return s;
+        })
         .collect()
 }
 
 // #[allow(dead_code)]
 // fn aoc_3_2(path: &str) {
-    
-// }
 
+// }
 
 #[derive(Debug, PartialEq, Eq, Hash)]
 struct Number {
     value: u32,
     line: usize,
     start_x: usize,
-    end_x: usize
+    end_x: usize,
 }
 
 fn matrix(path: &str) -> Vec<String> {
@@ -74,34 +87,34 @@ fn matrix(path: &str) -> Vec<String> {
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
     use super::*;
 
     const LINE_1: &str = "467..114..";
     const LINE_10: &str = ".664.598..";
-    const NO_114: Number  = Number {
+    const NO_114: Number = Number {
         value: 114,
         line: 0,
         start_x: 5,
-        end_x: 7
+        end_x: 7,
     };
     const NO_467: Number = Number {
         value: 467,
         line: 0,
         start_x: 0,
-        end_x: 2
+        end_x: 2,
     };
-    const NO_664: Number  = Number {
+    const NO_664: Number = Number {
         value: 664,
         line: 1,
         start_x: 1,
-        end_x: 3
+        end_x: 3,
     };
     const NO_598: Number = Number {
         value: 598,
         line: 1,
         start_x: 5,
-        end_x: 7
+        end_x: 7,
     };
 
     #[test]
