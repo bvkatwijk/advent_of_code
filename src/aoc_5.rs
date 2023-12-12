627130acf9d873526b51b7bbaf2ec4543839fbcf
Boris van Katwijk <bvkatwijk@gmail.com>
2023-12-12 23:29:43 +0100

    Update aoc_5.rs

--- src/aoc_5.rs
+++ src/aoc_5.rs
@@ -8,6 +8,7 @@ const EXAMPLE_01: &str = "./src/aoc_5/05_01_example.txt";
 #[allow(dead_code)]
 fn aoc_4_1(path: &str) -> i32 {
     use aoc_5::Resource::*;
+    let lines: Vec<String> = helper::file_lines(path).map(|l| l.unwrap()).collect();
 
     let resources = vec![Seed, Soil, Fertilizer, Water, Light, Temperature, Humidity, Location];
     0
@@ -25,9 +26,17 @@ enum Resource {
     Location
 }
 
+fn mapping_header<'a>(one: &'a Resource, other: &'a Resource) -> String {
+    format!("{:#?}-to-{:#?} map:", one, other)
+        .to_lowercase()
+}
+
 #[cfg(test)]
 mod tests{
     use super::*;
 
-
+    #[test]
+    fn mapping_header_test() {
+        assert_eq!("fertilizer-to-water map:", mapping_header(&Resource::Fertilizer, &Resource::Water));
+    }
 }
