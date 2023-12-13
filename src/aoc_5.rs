f93f45b57a57e938d140f39cabea0d3554df4a95
Boris van Katwijk <bvkatwijk@gmail.com>
2023-12-13 09:27:50 +0100

    Update aoc_5.rs

--- src/aoc_5.rs
+++ src/aoc_5.rs
@@ -1,4 +1,7 @@
+use std::{collections::HashMap, iter::FromIterator};
+
 use helper;
+use aoc_5::Resource::*;
 
 #[allow(dead_code)]
 const EXAMPLE_01: &str = "./src/aoc_5/05_01_example.txt";
@@ -7,10 +10,34 @@ const EXAMPLE_01: &str = "./src/aoc_5/05_01_example.txt";
 
 #[allow(dead_code)]
 fn aoc_4_1(path: &str) -> i32 {
-    use aoc_5::Resource::*;
-    let lines: Vec<String> = helper::file_lines(path).map(|l| l.unwrap()).collect();
+    let mut index = 0;
+    let mut line_groups: HashMap<u8, Vec<String>> = HashMap::new();
+    helper::file_lines(path)
+        .map(|l| l.unwrap())
+        .for_each(|s| {
+            println!("Processing {}", s);
+            if s.contains("map") {
+                index += 1;
+            }
 
-    let resources = vec![Seed, Soil, Fertilizer, Water, Light, Temperature, Humidity, Location];
+            if s.is_empty() {
+                return;
+            }
+
+            let mut vec = line_groups.entry(index).or_insert(vec![]);
+            vec.push(s);
+        });
+
+    // let seedToSoilMap: HashMap<u8, u8> = mapping()
+    // let resources = vec![Seed, Soil, Fertilizer, Water, Light, Temperature, Humidity, Location];
+    // let mappings: HashMap<>
+    // resources.windows(2)
+    //     .map(|r1, r2| )
+
+    for (key, value) in &line_groups {
+        println!("{}: {:#?}", key, value);
+    }
+
     0
 }
 
@@ -26,6 +53,34 @@ enum Resource {
     Location
 }
 
+enum ResourcePair {
+    SeedToSoil,
+    SoilToFertilizer,
+    FertilizerToWater,
+    WaterToLight,
+    LightToTemperature,
+    TemperatureToHumidity,
+    HumidityToLocation
+}
+
+struct Mapping {
+    dest: u32,
+    source: u32,
+    range: u32
+}
+
+fn from_and_to(pair: &ResourcePair) -> (Resource, Resource) {
+    match pair {
+        ResourcePair::SeedToSoil => (Seed, Soil),
+        ResourcePair::SoilToFertilizer => (Soil, Fertilizer),
+        ResourcePair::FertilizerToWater => (Fertilizer, Water),
+        ResourcePair::WaterToLight => (Water, Light),
+        ResourcePair::LightToTemperature => (Light, Temperature),
+        ResourcePair::TemperatureToHumidity => (Temperature, Humidity),
+        ResourcePair::HumidityToLocation => (Humidity, Location),
+    }
+}
+
 fn mapping_header<'a>(one: &'a Resource, other: &'a Resource) -> String {
     format!("{:#?}-to-{:#?} map:", one, other)
         .to_lowercase()
@@ -34,6 +89,11 @@ fn mapping_header<'a>(one: &'a Resource, other: &'a Resource) -> String {
 #[cfg(test)]
 mod tests{
     use super::*;
+
+    #[test]
+    fn aoc_4_1_test() {
+        assert_eq!(1, aoc_4_1(EXAMPLE_01));
+    }
 
     #[test]
     fn mapping_header_test() {
