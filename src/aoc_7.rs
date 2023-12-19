b6629df2e6edb602d1b71810a15a72b531fe80e4
Boris van Katwijk <bvkatwijk@gmail.com>
2023-12-19 07:47:31 +0100

    Update aoc_7.rs

--- src/aoc_7.rs
+++ src/aoc_7.rs
@@ -32,22 +32,38 @@ fn as_hand(s: &str) -> HandBid {
                 *map.entry(c).or_insert(0) += 1;
                 map
             }),
+        orig: s.to_string(),
         bid: split[1].parse::<u16>().unwrap(),
     }
 }
 
 struct HandBid {
     hand: HashMap<char, u8>,
+    orig: String,
     bid: u16
 }
 
 impl HandBid {
     fn compare(&self, other: &HandBid) -> Ordering {
-        self.hand_type_order(other)
+        self.hand_type_order(other).then(self.hand_card_order(other))
     }
 
+    // Compare hand type (e.g. four of a kind > full house)
     fn hand_type_order(&self, other: &HandBid) -> Ordering {
         self.hand.values().max().cmp(&other.hand.values().max())
+    }
+
+    // Compare hand card (e.g. A > K)
+    fn hand_card_order(&self, other: &HandBid) -> Ordering {
+        hand_card_compare(&self.orig, &other.orig)
+    }
+}
+
+fn hand_card_compare(one: &str, other: &str) -> Ordering {
+    match one.len()  {
+        1 => card_compare(one, other),
+        _ => card_compare(&one[0..1], &other[0..1])
+            .then(hand_card_compare(&one[1..], &other[1..]))
     }
 }
 
@@ -79,6 +95,17 @@ mod tests {
         assert_eq!(Ordering::Less, two.hand_type_order(&one));
         assert_eq!(Ordering::Equal, one.hand_type_order(&one));
         assert_eq!(Ordering::Equal, two.hand_type_order(&three));
+    }
+
+    #[test]
+    fn hand_card_order_test() {
+        let one = as_hand("AAAAA 0");
+        let two = as_hand("AAAAK 0");
+        let three = as_hand("KKKKA 0");
+        assert_eq!(Ordering::Greater, one.hand_card_order(&two));
+        assert_eq!(Ordering::Less, two.hand_card_order(&one));
+        assert_eq!(Ordering::Equal, one.hand_card_order(&one));
+        assert_eq!(Ordering::Equal, two.hand_card_order(&three));
     }
 
     #[test]
