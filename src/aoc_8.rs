d51cacf97d874d51b3a0b897d9663ae04cbc7707
Boris van Katwijk <bvkatwijk@gmail.com>
2024-01-01 12:02:53 +0100

    Update aoc_8.rs

--- src/aoc_8.rs
+++ src/aoc_8.rs
@@ -1,6 +1,3 @@
-use core::panic;
-use std::{cmp::Ordering, collections::HashMap};
-
 use crate::helper::{self};
 
 #[allow(dead_code)]
@@ -10,10 +7,45 @@ const EXAMPLE_02: &str = "./src/aoc_8/example_02.txt";
 
 #[allow(dead_code)]
 fn aoc_8_1(path: &str) -> usize {
-    let mut lines: Vec<String> = helper::file_lines(path)
+    let lines: Vec<String> = helper::file_lines(path)
         .map(|l| l.unwrap())
         .collect();
+    let instructions: &str = &lines[0];
+    let network = network(&lines[2..]);
     0
+}
+
+fn network(lines: &[String]) -> Vec<Node> {
+    lines.iter()
+        .map(|s| as_node(s))
+        .collect()
+}
+
+fn as_node(s: &str) -> Node {
+    let split: Vec<&str> = s.split("=")
+        .map(|s| s.trim())
+        .collect();
+    let trimmed = split[1]
+        .replace(&['(', ')'][..], "");
+    let left_right: Vec<&str> = trimmed
+        .split(",")
+        .map(|s| s.trim())
+        .collect();
+    Node {
+        name: split[0].to_owned(),
+        left: left_right[0].to_owned(),
+        right: left_right[1].to_owned(),
+    }
+}
+
+#[derive(Debug, Hash, PartialEq)]
+struct Node {
+    name: String,
+    left: String,
+    right: String
+}
+
+impl Node {
 }
 
 #[cfg(test)]
@@ -23,5 +55,14 @@ mod tests {
     #[test]
     fn aoc_8_1_test() {
         assert_eq!(2, aoc_8_1(EXAMPLE_01));
+    }
+
+    #[test]
+    fn node_test() {
+        assert_eq!(Node {
+            name: "AAA".to_owned(),
+            left: "BBB".to_owned(),
+            right: "CCC".to_owned(),
+        }, as_node("AAA = (BBB, CCC)"))
     }
 }
