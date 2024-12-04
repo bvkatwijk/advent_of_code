09e333f697b0bc9b129f852aa51e78c5e46c7771
Boris van Katwijk <bvkatwijk@gmail.com>
2024-12-04 17:12:52 +0100

    Update 04.rs

--- 2024/src/bin/04.rs
+++ 2024/src/bin/04.rs
@@ -1,8 +1,30 @@
 advent_of_code::solution!(4);
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<usize> {
     let xmas = "XMAS";
-    None
+    Some(
+        count_horizontal(input)
+            + count_horizontal(rotate(input))
+            + count_horizontal(diag(input))
+            + count_horizontal(rotate(diag(input)))
+    )
+    // TODO how to rotate ??
+    // TODO diag -> how to ??
+}
+
+// both xmas and reverse (samx)
+pub fn count_horizontal(input: &str) -> usize {
+    input.lines()
+        .map(|l| l.split("XMAS").count() - 1 + l.split("SAMX").count() - 1)
+        .sum()
+}
+
+pub fn rotate(input: &str) -> &str {
+    ""
+}
+
+pub fn diag(input: &str) -> &str {
+    ""
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
