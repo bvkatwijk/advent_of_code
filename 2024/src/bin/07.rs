dc6de2338062b34aeb448710053009655b435a8b
Boris van Katwijk <bvkatwijk@gmail.com>
2024-12-22 16:43:10 +0100

    Update 07.rs

--- 2024/src/bin/07.rs
+++ 2024/src/bin/07.rs
@@ -2,11 +2,11 @@ advent_of_code::solution!(7);
 
 pub fn part_one(input: &str) -> Option<u64> {
     Some(input.lines()
-        .filter_map(|l| calibration(l))
+        .filter_map(|l| calibration(l, false))
         .sum())
 }
 
-pub fn calibration(input: &str) -> Option<u64> {
+pub fn calibration(input: &str, with_concat: bool) -> Option<u64> {
     let mut s = input.split(":");
     let sum  = str::parse::<u64>(s.next().unwrap()).ok().unwrap();
     let vals: Vec<u64> = s.next()
@@ -14,7 +14,9 @@ pub fn calibration(input: &str) -> Option<u64> {
         .map(|s| str::parse::<u64>(s).ok().unwrap())
         .collect();
 
-    if combination(vals[0], sum, &vals[1..]) {
+    if !with_concat && combination(vals[0], sum, &vals[1..]) {
+        return Some(sum);
+    } else if with_concat && combination_with_concat(vals[0], sum, &vals[1..]) {
         return Some(sum);
     } else {
         return None;
@@ -37,8 +39,33 @@ pub fn combination(current: u64, target: u64, vals: &[u64]) -> bool {
         || combination(current * next, target, &remaining);
 }
 
+pub fn combination_with_concat(current: u64, target: u64, vals: &[u64]) -> bool {
+    if current == target && vals.len() < 1 {
+        return true;
+    }
+    if current > target {
+        return false;
+    }
+    if vals.len() < 1 {
+        return false;
+    }
+    let next: u64 = vals[0];
+    let remaining = &vals[1..];
+    return combination_with_concat(current + next, target, &remaining)
+        || combination_with_concat(current * next, target, &remaining)
+        || combination_with_concat(concat(current, next), target, &remaining);
+}
+
+pub fn concat(one: u64, other: u64) -> u64 {
+    str::parse::<u64>(&(one.to_string() + &other.to_string()))
+        .ok()
+        .unwrap()
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    Some(input.lines()
+        .filter_map(|l| calibration(l, true))
+        .sum())
 }
 
 #[cfg(test)]
@@ -47,7 +74,12 @@ mod tests {
 
     #[test]
     fn test_calibration() {
-        assert_eq!(calibration("190: 10 19"), Some(190));
+        assert_eq!(calibration("190: 10 19", false), Some(190));
+    }
+
+    #[test]
+    fn test_concat() {
+        assert_eq!(concat(1, 2), 12);
     }
 
     #[test]
@@ -59,6 +91,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(11387));
     }
 }
