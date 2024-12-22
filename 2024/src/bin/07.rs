f1629451df8710ea49c3b560ec84a853706a9122
Boris van Katwijk <bvkatwijk@gmail.com>
2024-12-22 16:22:17 +0100

    out of bounds, use u64 but too high answer

--- 2024/src/bin/07.rs
+++ 2024/src/bin/07.rs
@@ -1,17 +1,17 @@
 advent_of_code::solution!(7);
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<u64> {
     Some(input.lines()
         .filter_map(|l| calibration(l))
         .sum())
 }
 
-pub fn calibration(input: &str) -> Option<u32> {
+pub fn calibration(input: &str) -> Option<u64> {
     let mut s = input.split(":");
-    let sum  = str::parse::<u32>(s.next().unwrap()).ok().unwrap();
-    let vals: Vec<u32> = s.next()
+    let sum  = str::parse::<u64>(s.next().unwrap()).ok().unwrap();
+    let vals: Vec<u64> = s.next()
         ?.split_whitespace()
-        .map(|s| str::parse::<u32>(s).ok().unwrap())
+        .map(|s| str::parse::<u64>(s).ok().unwrap())
         .collect();
 
     if combination(vals[0], sum, &vals[1..]) {
@@ -21,7 +21,7 @@ pub fn calibration(input: &str) -> Option<u32> {
     }
 }
 
-pub fn combination(current: u32, target: u32, vals: &[u32]) -> bool {
+pub fn combination(current: u64, target: u64, vals: &[u64]) -> bool {
     if current == target {
         return true;
     }
@@ -31,13 +31,13 @@ pub fn combination(current: u32, target: u32, vals: &[u32]) -> bool {
     if vals.len() < 1 {
         return false;
     }
-    let next: u32 = vals[0];
+    let next: u64 = vals[0];
     let remaining = &vals[1..];
     return combination(current + next, target, &remaining)
         || combination(current * next, target, &remaining);
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<u64> {
     None
 }
 
